2531cbd310dbe6f5a9daa4e1757670c3e891d734
Radu M <root@radu.sh>
2019-06-04 17:23:53 +0300

    Create small test app and add test data
    Signed-off-by: Radu M <root@radu.sh>

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+
+extern crate serde_canonical;
+extern crate serde_json;
+
+use std::{env, fs, path};
+
+const ARG_PANIC_MESSAGE :&str = "The first argument is the input JSON, and the second is an optional output file.";
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        2 => {
+            let input = &args[1];
+            let input = fs::File::open(path::Path::new(input)).expect("cannot open input file");
+            let res: serde_json::Value = serde_json::from_reader(input).expect("cannot deserialize input file");
+            println!("{}", serde_canonical::ser::to_string(&res).expect("cannot write canonical JSON"));
+        },
+        3 => {
+            let input = &args[1];
+            let output = &args[2];
+
+            let input = fs::File::open(path::Path::new(input)).expect("cannot open input file");
+            let mut output = fs::File::create(path::Path::new(output)).expect("cannot create or open output file");
+            let res: serde_json::Value = serde_json::from_reader(input).expect("cannot deserialize input file");
+            serde_canonical::ser::to_writer(&mut output, &res).expect("cannot write canonical JSON");
+        },
+        _ => panic!(ARG_PANIC_MESSAGE)
+    };
+}
